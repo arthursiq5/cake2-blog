99f3b81e80f0642deb7eac287afc86054752d43e
Arthur Siqueira <arthur.siq5@gmail.com>
2021-03-05 15:13:05 -0300

    Criada estrutura para posts

---
+++ Controller/PostsController.php
@@ -0,0 +1,25 @@
+<?php
+
+class PostsController extends AppController
+{
+    public $helpers = array('Html', 'Form', 'Flash');
+    public $components = array('Flash');
+    public $name = 'Posts';
+
+    function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id = null) {
+        $this->set('post', $this->Post->findById($id));
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            if ($this->Post->save($this->request->data)) {
+                $this->Flash->success('Your post has been saved.');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
+}
